20d33866b17514315cea99e476f935efe7c41abf
Muadfi-Nabil <muadfinabil06@gmail.com>
2025-12-11 15:43:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,16 @@ edited_df = st.data_editor(df, num_rows="dynamic")
 
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+
+st.line_chart(
+    df,
+    x="a",
+    y=["b", "c"],
+    color=["#FF0000", "#0000FF"],
+)
