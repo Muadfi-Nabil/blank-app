2146d216b6d7589d240ba3efd3095a28f87c982e
Muadfi-Nabil <muadfinabil06@gmail.com>
2025-12-11 15:28:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My project LPK Nanoteknologi Pangan")
-st.write(
+st.write("NTP IS COOL")
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
