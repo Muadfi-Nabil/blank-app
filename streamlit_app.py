19cbfe91f3bf8fae4c4054cb75d0271cecc7b96b
Muadfi-Nabil <muadfinabil06@gmail.com>
2025-12-11 15:48:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,34 +3,6 @@ import streamlit as st
 st.title("🎈 My project LPK Nanoteknologi Pangan")
 
 import streamlit as st
-import pandas as pd
-
-df = pd.DataFrame(
-    [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
-    ]
-)
-edited_df = st.data_editor(df, num_rows="dynamic")
-
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
-
-import pandas as pd
-import streamlit as st
-from numpy.random import default_rng as rng
-
-df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
-
-st.line_chart(
-    df,
-    x="a",
-    y=["b", "c"],
-    color=["#FF0000", "#0000FF"],
-)
-
-import streamlit as st
 
 pages = {
     "Your account": [
