1301d53663f2c3980aa2a5ee2d5180515933be17
Muadfi-Nabil <muadfinabil06@gmail.com>
2025-12-11 15:30:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
 st.title("🎈 My project LPK Nanoteknologi Pangan")
-st.write("NTP IS COOL")
+import streamlit as st
+import pandas as pd
+
+st.write(1234)
+st.write(
+    pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+)
+st.write()
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
